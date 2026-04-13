1e2033226a24334882d020c1bfe82072ec7b4959
Ferrizae <hardizaenuk@gmail.com>
2026-04-13 12:07:43 +0800

    commit pertamaku

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+ $nama = "Mahfud";
+ $umur = 20;
+ $berat = 48.5;
+ $menikah = false;
+ $hobi = ["read", "swim", "football"];
+echo "nama saya $nama, umur saya $umur, berat saya $berat, status saya $menikah, hobi saya $hobi[1]";
+?>
